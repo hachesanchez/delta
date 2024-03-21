e4fc6410959553bc1fd6df866c4754e113ec1dbe
Helena Sánchez <helena.sangar@gmail.com>
2024-03-21 19:30:10 +0100

    hero + team page begin

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from '../../team.json'
 import './TeamPage.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function TeamPage() {
+
+    const [selected, setSelected] = useState()
+
+    const handleSelected = (name) => {
+        setSelected(name)
+        console.log(selected)
+    }
+
     return (
 
         <>
-            <div className='team-page'>TeamPage</div>
+            <div className="team-page">
+                <p className='page-title'>
+                    Conoce al equipo
+                </p>
+            </div>
+
+            <Container>
+                <Row>
+
+                    <Col md={3} sm={12}>
+                        {data?.map((e) => (
+                            <div
+                                key={e.name}
+                                onClick={handleSelected}
+                                className='team-name'
+                            >
+                                {e.name}
+                            </div>
+                        ))}
+                    </Col>
+
+                    <Col md={9} sm={12}>
+                        <p className="team-intro">
+                            Delta es un Centro de Diagnóstico por la Imagen dirigido por Pilar Martínez-Ten y Carmina Bermejo formado por un equipo de profesionales altamente cualificados y con amplia experiencia en diagnóstico prenatal así como en patología ginecológica y de mama.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+
+
         </>
     )
 }
